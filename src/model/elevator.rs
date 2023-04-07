342a539e39d8d4c08e8e33699059d9522b0c4a62
andrew <andrew@node.rip>
2023-04-07 12:33:44 +0000

    Working on model a bit

--- src/model/elevator.rs
+++ src/model/elevator.rs
@@ -2,6 +2,8 @@ struct Elevator {
     name: String,
     curr_floor: int,
     dir: Direction,
+    max_floor: int,
+    min_floor: int,
 }
 
 enum Direction {
@@ -15,5 +17,28 @@ impl Elevator {
     // Constructor
     fn new(name: String) -> Elevator {
         return Elevator {name: start_floor, curr_floor: 0, dir: Direction::Stop}
+    }
+
+    // Move elevator X floors in a Direction
+    fn move(&self, target_floor: int) {
+        if target_floor > max_floor || target_floor < 0 {
+            // Throw exception
+        }
+
+        if target_floor > start_floor {
+            self.dir = Direciton.Up;
+        } else if target_floor < start_floor {
+            self.dir = Direciton.Down;
+        }
+
+        ratchet(target_floor - start_floor);
+        self.dir = Direction.Stop;
+    }
+
+    // Go up each floor with a consistent speed
+    fn rachet(&self, disp: int) {
+        while () {
+
+        }
     }
 }
